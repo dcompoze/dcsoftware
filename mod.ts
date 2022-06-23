44f25a8fd235174b6337655e718ca7dda49a02f8
Daniel Chmielewski <contact@dcsoftware.xyz>
2022-06-23 23:12:35 +0100

    Added: 0 | Modified: 1 | Deleted: 0 | #744623

--- mod.ts
+++ mod.ts
@@ -1,20 +1,13 @@
 import { serve } from "https://deno.land/std@0.145.0/http/server.ts";
-import { serveFile } from "https://deno.land/std@0.145.0/http/file_server.ts";
+import { serveDir, serveFile } from "https://deno.land/std@0.145.0/http/file_server.ts";
 
 async function handleRequest(request: Request): Promise<Response> {
     const { pathname } = new URL(request.url);
-    if (pathname == "/" || pathname.startsWith("/index.html")) {
+
+    if (pathname == "/") {
         return await serveFile(request, `${Deno.cwd()}/static/index.html`);
-    } else if (pathname.startsWith("/styles.css")) {
-        return await serveFile(request, `${Deno.cwd()}/static/styles.css`);
-    } else if (pathname.startsWith("/favicon/favicon.ico")) {
-        return await serveFile(request, `${Deno.cwd()}/static/favicon/favicon.ico`);
-    } else if (pathname.startsWith("/favicon/apple-touch-icon.png")) {
-        return await serveFile(request, `${Deno.cwd()}/static/favicon/apple-touch-icon.png`);
-    } else if (pathname.startsWith("/index.js")) {
-        return await serveFile(request, `${Deno.cwd()}/static/index.js`);
     } else {
-        return await serveFile(request, `${Deno.cwd()}/static/index.html`);
+        return await serveDir(request, { fsRoot: `${Deno.cwd()}/static` });
     }
 }
 
